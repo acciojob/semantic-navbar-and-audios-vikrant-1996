99bcae9256ae9b25193bd39655060fb766df2799
vikrant-1996 <159111034+vikrant-1996@users.noreply.github.com>
2024-09-14 03:27:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-describe('Audio Section Test', () => {
-    it('should display the correct heading', () => {
-        cy.visit('http://localhost:3000'); // Replace with your actual URL
-        cy.contains('h2', '3 Random Audios'); // Ensure correct text and tag
-    });
-});
+
